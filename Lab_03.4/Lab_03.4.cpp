8ff2c51d79a15ae38242b3cc137a7f8b92f0f3ae
Taras <tarashuledza14@gmail.com>
2022-10-13 13:48:47 +0300

    lab 3.4

---
+++ Lab_03.4/Lab_03.4.cpp
@@ -0,0 +1,29 @@
+﻿// Lab_03_4.cpp
+// Гуледза Тарас
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 6
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x;
+	double y;
+	double R;
+
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+	  
+	if ((pow(x-R,2)+pow(y,2) <= pow(R,2) && y >= 0) || (pow(x+R,2)+pow(y+R,2) >= pow(R,2) && y<=0))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+
+	cin.get();
+	return 0;
+}
